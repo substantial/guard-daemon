e88770a458b2227fd823d3cb5ac6f23e27144334
re5et <re5etsmyth@gmail.com>
2012-09-17 11:25:18 -0700

    Convert structure to guard plugin

---
+++ lib/guard/daemon.rb
@@ -0,0 +1,58 @@
+require "guard-daemon/version"
+
+module Guard
+
+  class Daemon < Guard
+
+    def initialize(watchers = [], options = [])
+      super
+    end
+
+    def start
+      daemon_restart
+    end
+
+    def run_on_changes(paths)
+      daemon_restart
+    end
+
+    def stop
+      daemon_stop
+    end
+
+    def daemon_restart
+      daemon_stop
+      daemon_start
+    end
+
+    def daemon_stop
+      return unless @pid
+      rows = `ps -eo pid,ppid | grep #{@pid}`.split("\n")
+      rows.each do |row|
+        pids = row.split(' ')
+        if pids[1] == @pid.to_s
+          `kill -9 #{pids[0]}`
+        end
+      end
+    end
+
+    def daemon_start
+      @pid = fork do
+        cmd = @options[:command]
+        daemon_logger "starting with '#{cmd}'"
+        exec cmd
+      end
+      daemon_logger "made pid #{@pid}"
+    end
+
+    def daemon_logger(msg)
+      name = @options[:name]
+      output = "DAEMON".color(:yellow)
+      output << "[" + name.color(:blue) + "]"
+      output << " #{msg}"
+      puts output
+    end
+
+  end
+
+end
---
+++ lib/guard/daemon/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Daemon
+    VERSION = "0.0.1"
+  end
+end
